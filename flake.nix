cf6708279cbcd9a1ac4f234fc53b6ece8bf63b3f
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-12 15:44:23 +0300

    feat: add nix build files (flakes==experimental, default==legacy)

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-20.09";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+
+	outputs = inputs: 
+	let
+		inherit (inputs.flake-utils.lib) eachDefaultSystem flattenTree mkApp;
+	in
+	eachDefaultSystem (system:
+      let
+	 	pkgs = inputs.nixpkgs.legacyPackages.${system};
+	  	lib = inputs.nixpkgs.lib;
+	  in
+      rec {
+        defaultPackage = (import ./default.nix) {
+			inherit pkgs lib system;
+		};
+      }
+    );
+}
